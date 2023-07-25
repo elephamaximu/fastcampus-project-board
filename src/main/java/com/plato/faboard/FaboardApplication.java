8a94d736b9171b4e158cbb50d53bbe09ebd6d651
elephamaximu <elephamaximu@gmail.com>
2023-07-25 17:56:08 +0900

    first commit

---
+++ src/main/java/com/plato/faboard/FaboardApplication.java
@@ -0,0 +1,13 @@
+package com.plato.faboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plato/faboard/FaboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plato.faboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
